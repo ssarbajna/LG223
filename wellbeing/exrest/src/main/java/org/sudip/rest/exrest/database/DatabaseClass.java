667e3338b67d137dd432a7bb21eff11c4be0027b
SUDIPnoadm <SUDIPnoadm@10.0.0.44>
2018-02-02 22:01:07 -0500

    progress on REST

--- wellbeing/exrest/src/main/java/org/sudip/rest/exrest/database/DatabaseClass.java
+++ wellbeing/exrest/src/main/java/org/sudip/rest/exrest/database/DatabaseClass.java
@@ -1,5 +1,21 @@
 package org.sudip.rest.exrest.database;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.sudip.rest.exrest.model.Inventory;
+import org.sudip.rest.exrest.model.TransactionOfAnInventory;
+
 public class DatabaseClass {
+	private static Map<Long,Inventory> inventories = new HashMap<Long,Inventory>();
+	private static Map<Long,TransactionOfAnInventory> transactionsOfInventories = new HashMap<Long,TransactionOfAnInventory>();
+	
+	public static Map<Long,Inventory> getInventories(){
+		return inventories;
+	}
+	
+	public static Map<Long,TransactionOfAnInventory> getTransactionsOfInventories(){
+		return transactionsOfInventories;
+	}
 
 }
--- wellbeing/exrest/src/main/java/org/sudip/rest/exrest/service/InventoryService.java
+++ wellbeing/exrest/src/main/java/org/sudip/rest/exrest/service/InventoryService.java
@@ -2,18 +2,38 @@ package org.sudip.rest.exrest.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.sudip.rest.exrest.database.DatabaseClass;
 import org.sudip.rest.exrest.model.Inventory;
 
 public class InventoryService {
+	private Map<Long, Inventory> inventories = DatabaseClass.getInventories();
+	
 	public List<Inventory> getAllInventories(){
-		Inventory i1 = new Inventory(1L,"Hello World!", "Sudip");
-		Inventory i2 = new Inventory(2L,"Hello Jersey", "Sudip");
-		List<Inventory> list = new ArrayList<Inventory>();
-		list.add(i1);
-		list.add(i2);
 		
-		return list;
-		};
-
+		return new ArrayList<>(inventories.values());
+	}
+	
+	public Inventory getInventory(long id) {
+		return inventories.get(id);
+	}
+	
+	public Inventory addInventory(Inventory inv) {
+		inv.setId(inventories.size() + 1);
+		inventories.put(inv.getId(), inv);
+		return inv;
+	}
+	
+	public Inventory updateInventory(Inventory inv) {
+		if (inv.getId() <= 0)
+			return null;
+		
+		inventories.put(inv.getId(), inv);
+		return inv;
+	}
+	
+	public Inventory removeInventory(long id) {
+			return inventories.remove(id);
+	}
 }
